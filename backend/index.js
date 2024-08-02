290ba7b941103dc8f256b2325d8a83b14de8a6b2
Abhinandan <jhaabhinandan55@gmail.com>
2024-08-02 18:49:31 +0530

    backend setup done

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/bfhl", (req, res) => {
+  res.status(200).json({ operation_code: 1 });
+});
+
+app.post("/bfhl", (req, res) => {
+  const { data } = req.body;
+  const user_id = "abhinandan_jha_05062002";
+  const email = "jhaabhinandan55@gmail.com";
+  const roll_number = "RA2111051010006";
+
+  let numbers = [];
+  let alphabets = [];
+
+  data.forEach((item) => {
+    if (!isNaN(item)) {
+      numbers.push(item);
+    } else {
+      alphabets.push(item);
+    }
+  });
+
+  alphabets.sort((a, b) =>
+    b.localeCompare(a, undefined, { sensitivity: "base" })
+  );
+  const highest_alphabet = alphabets.length > 0 ? [alphabets[0]] : [];
+
+  res.json({
+    is_success: true,
+    user_id,
+    email,
+    roll_number,
+    numbers,
+    alphabets,
+    highest_alphabet,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
